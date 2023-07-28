49c5dfc4cde5624081a248ce9813546a2bbace8b
Rin Brk <dareka826@gmail.com>
2023-07-28 18:59:21 +0200

    Add response check

--- prog.cs
+++ prog.cs
@@ -5,6 +5,12 @@ static class MainClass {
     public static void Main(string[] args) {
         using (var client = new HttpClient()) {
             var response = client.GetAsync("https://ddg.gg/lite").Result;
+
+            if (response.IsSuccessStatusCode != true) {
+                Console.WriteLine($"[E]: ({response.StatusCode}) {response.ReasonPhrase}");
+                Environment.Exit(1);
+            }
+
             var result = response.Content.ReadAsStringAsync().Result;
 
             Console.WriteLine(result);
